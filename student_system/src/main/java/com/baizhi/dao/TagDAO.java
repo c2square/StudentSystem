80fedc0255aee2548b8dafb662b10b46adc84409
1313113 <32981640+1313113@users.noreply.github.com>
2020-06-03 06:42:00 -0500

    增加@Repotisory注解，作为一个可被扫描的组件

--- student_system/src/main/java/com/baizhi/dao/TagDAO.java
+++ student_system/src/main/java/com/baizhi/dao/TagDAO.java
@@ -2,10 +2,12 @@ package com.baizhi.dao;
 
 import com.baizhi.entity.Tag;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface TagDAO extends BaseDAO<Tag,String> {
 
 
